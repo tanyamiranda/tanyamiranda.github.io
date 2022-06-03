1a991a180bcbfd8059c92761ade9819e7191b009
Tanya Miranda <tanya.miranda@gmail.com>
2022-06-02 22:21:24 -0400

    Removed all buy links except for Amazon

--- content/scripts/books.js
+++ content/scripts/books.js
@@ -44,11 +44,12 @@ const BOOK_META_DATA = [
         fullblurb: "Jubilee Ray wakes up Saturday morning in a strange bed with a handsome guy asleep at her side and no memory of how she got there. Everett Salerno wakes up ecstatic about the incredible night he spent with Jubilee. She can’t wait to get out of his apartment and he can’t wait to start their romance. Jubilee is a blackout drunk. Everett is drunk with love.<br/><br/>Their one-night-stand ends almost as quickly as it began, however fate has other plans in mind. In the next three days filled with impossible coincidences, humiliating moments, and mouthwatering recollections, Everett can’t find the right moment to express his intentions and Jubilee can’t let go of her past long enough to really give Everett a chance.<br/><br/>Neither Jubilee nor Everett can let go of that first night together. Was that one perfect evening a fluke, or was it a preview of something greater than they ever imagined?",
         shoplink: "https://bookstore.tanyamiranda.com/b/jthdG",
         amazon: "http://www.amzn.to/1mvkEan",
-        nook: "http://www.bit.ly/2MCTcF7",
+        /*nook: "http://www.bit.ly/2MCTcF7",
         ibooks: "http://www.apple.co/2tTnZpy",
         google: "http://www.bit.ly/2tKixpC",
         smashwords: "http://www.bit.ly/2KF9MH4",
         kobo: "http://www.bit.ly/2tJICp7"
+        */
     },
     {
         id:"isabel-and-leo",
@@ -60,27 +61,29 @@ const BOOK_META_DATA = [
         fullblurb: "Finding romance gets harder after so many years together, but Isabel and Leo make it work. This collection of short stories takes a peek into their lives as a competitive couple continually outwitting one another, as parents to two children who are too wise for their young ages, and as grownups who shoot foam dart guns like military snipers while attempting to act like responsible adults.<br/><br/>Between sassy kids and demanding careers, heartbreaks and frustrations, a quick kiss or a love tap are sometimes the only intimacy they can get. And when the moment comes, they jump all over it.<br/><br/>These are their stories.",
         shoplink: "https://bookstore.tanyamiranda.com/b/JoDKc",
         amazon: "http://www.amzn.to/2lKfPvX",
-        nook: "http://www.bit.ly/2tH0rVO",
+        /*nook: "http://www.bit.ly/2tH0rVO",
         ibooks: "http://www.apple.co/2MF3QeF",
         google: "http://www.bit.ly/2NgLr95",
         smashwords: "http://www.bit.ly/2lJvpYR",
         kobo: "http://www.bit.ly/2lNyibb"
+        */
     },
     {
         id:"distant-origins",
         title: "Distant Origins",
         subtitle: "",
         image:"/images/books-DistantOrigins.jpg",
-        tagline: "Where you come from shouldn't matter. Unless is does.",
+        tagline: "",
         shortblurb: "<p>Can Sarah Winters save the only other surviving Dramani female on Earth, or will she be the last of her kind? Only her powerful telekinetic ability can stop the destruction of her species, but is it enough to save herself?</p>",
         fullblurb: "Sarah Winters is an orphan. Her origin has never interested her until she meets Darren McAvoy and can communicate with him telepathically. They are of a human-like alien species called the Dramani who possess advanced physical and mental abilities. Sarah's a late bloomer for her kind, but on her twenty-fifth birthday she finds out just how powerful Dramani women can be.<br/><br/>As she digs further into what it means to be Dramani, Sarah learns the women are being targeted for elimination. She joins a top-secret military mission to rescue the only other surviving woman in the heart of Death Valley and finds there is more to the disappearances than the military knows.<br/><br/>Only Sarah's powerful telekinetic ability can stop the destruction of her species, but will it be enough to save herself?",
         shoplink: "https://bookstore.tanyamiranda.com/b/QHnsd",
-        amazon: "http://www.amzn.to/2z9A8Nd",
-        nook: "http://www.bit.ly/2KAZmIB",
+        amazon: "https://www.amazon.com/dp/B0B343T3BN",
+        /*nook: "http://www.bit.ly/2KAZmIB",
         ibooks: "http://www.apple.co/2tHbGxu",
         google: "http://www.bit.ly/2NdVzzv",
         smashwords: "http://www.bit.ly/2z65eVY",
         kobo: "http://www.bit.ly/2KyQzXI"
+        */
     },
 
 ];
@@ -132,10 +135,13 @@ function getBookDetailsHTML(book, cssClass, shortBlurb) {
                         "<div class='book-details-subtitle'>" + book.subtitle + "</div>" + 
                         "<div class='book-details-tagline'>" + book.tagline + "</div>" + 
                         "<div class='book-details-shortblurb'>" + (shortBlurb ? book.shortblurb : book.fullblurb) + "</div>"       
-                        
+                                                
+                        html += "<div class='myStoreButton'><a href='" + book.amazon + "' target='_blank'>Buy now on Amazon!</a></div>";
+
+                        /*
                         if(book.shoplink !=null && book.shoplink !== "") {
                             html += "<div class='myStoreButton'><a href='" + book.shoplink + "' target='_blank'>Buy the eBook in my shop!</a></div>";
-                        }            
+                        } 
 
                         html += "<div class='book-links'><p>Get your copy from your favorite bookseller:</p>" + 
                             "<a href='" + book.amazon + "' target='_blank'><img class='bookSellerButton' src='" + contentlocation + "/images/store-amazon.png'/></a>" + 
@@ -144,7 +150,9 @@ function getBookDetailsHTML(book, cssClass, shortBlurb) {
                             "<a href='" + book.kobo + "' target='_blank'><img class='bookSellerButton' src='" + contentlocation + "/images/store-nook.png'/></a>" +
                             "<a href='" + book.nook + "' target='_blank'><img class='bookSellerButton' src='" + contentlocation + "/images/store-kobo.png'/></a>" +
                             "<a href='" + book.smashwords + "' target='_blank'><img class='bookSellerButton' src='" + contentlocation + "/images/store-smashwords.png'/></a>" +
-                        "</div>" +        
+                        */
+                        
+                        html += "</div>" +        
                     "</div>" + 
                 "</div>" + 
             "</div>";
