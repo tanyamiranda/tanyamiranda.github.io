0251c4c4c4c020fe2d87606b35b38b08a741dfcb
Tanya Miranda <tanya.miranda@gmail.com>
2023-12-19 11:34:57 -0500

    Updating js code to accept hashtags to link externally. Needed to update a style for specific content section since ids of content section changed.

--- apitherapy/script.js
+++ apitherapy/script.js
@@ -25,3 +25,53 @@ function menuSelect(selectedItem) {
         }
     }
 }
+
+
+function displaySection(sectionName) {
+
+	const HOMEPAGETAG="welcome";
+	const ALLTAGS=["welcome","apitherapy", "products-and-services", "about-me"];
+	
+	let sectionTag = "";
+	
+	if (sectionName==='' || ALLTAGS.indexOf(sectionName)===-1) {
+		sectionTag= HOMEPAGETAG;
+        window.location.hash = "welcome";
+    }
+	else
+        sectionTag = sectionName;
+
+    sectionTag = sectionTag + "-section";
+    console.log("sectionName=" + sectionName);
+    console.log("sectionTag=" + sectionTag);
+
+    var contentSections=document.getElementsByClassName("content-section");
+    var menuItems=document.getElementsByClassName("menu-items");
+
+    for(var x=0; x < contentSections.length; x++) {
+        
+        var sectionId = contentSections[x].id;
+
+        if(sectionId === sectionTag) {
+            contentSections[x].style.display = "inline-block";
+            menuItems[x].style.color = "yellow";
+        }
+        else {
+            contentSections[x].style.display = "none";
+            menuItems[x].style.color = "white";
+        }
+    }
+
+}
+
+/******************************
+In case the sections are linked
+externally, pre-load section
+******************************/
+const hash = window.location.hash;
+displaySection(hash.substring(1));
+
+window.onpopstate = function() {
+	const hash = window.location.hash;
+	displaySection(hash.substring(1));
+}
